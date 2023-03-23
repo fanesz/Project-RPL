932d2721eabda456d3f90c0d397b67728a6bf8ea
Vanezkun <fanes23.pratama@mhs.mdp.ac.id>
2023-03-23 13:23:22 +0700

    + generator2

--- backend/utils/utils.js
+++ backend/utils/utils.js
@@ -6,9 +6,27 @@ const generateVerificationCode = (len) => {
 
 };
 
+const generateVerificationCode2 = (len=6, letter=false, num=true) => {
+    if (num == false && letter == false || len < 1) {
+      throw new Error("Invalid input parameters");
+    }
+    let chars = "";
+    let code = "";
+    if (num && letter) {
+      chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    } else if (num) {
+      chars = "0123456789";
+    } else if (letter) {
+      chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    }
+    for (let i = 0; i < len; i++) {
+      code += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+    return code;
+}
 
 
 
 
-export { generateVerificationCode  }
+export { generateVerificationCode, generateVerificationCode2  }
   
