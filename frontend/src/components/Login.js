238eccd4cc52aa47c0d2e680b98a8dc483acbfa5
VanezKun <fanes23.pratama@mhs.mdp.ac.id>
2023-05-27 09:35:05 +0700

    1

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,10 +1,10 @@
 import { useState } from 'react';
 import axios from 'axios';
 import { isLogin, setLoginCookie } from '../utils/utils'
-import { useNavigate } from 'react-router-dom';
 
 
 const Login = () => {
+
 if(isLogin()){
     window.location.href = "/";
 }
@@ -13,11 +13,6 @@ const [password, setPassword] = useState('');
 const [email2, setEmail2] = useState('');
 const [password2, setPassword2] = useState('');
 const [email3, setEmail3] = useState('');
-
-const [showLoginPage, setShowLoginPage] = useState(true);
-const [showForgetPassword, setShowForgetPassword] = useState(false);
-const [showDaftar, setShowDaftar] = useState(false);
-
 
 const createAccount = async (e) => {
     e.preventDefault();
@@ -59,6 +54,7 @@ const sendVerificationCode = async (e) => {
     })
 }
 
+<<<<<<< HEAD
 const login = () => {
     setShowLoginPage(true);
     setShowForgetPassword(false);
@@ -78,201 +74,11 @@ const register = () => {
     setShowDaftar(true);
 
 }
+=======
+>>>>>>> parent of 2d73773 (7)
 
 return (
 <div>
-<style jsx global>{`
-* {
-    margin: 0;
-    padding: 0;
-    outline: 0;
-    font-family: "Open Sans", sans-serif;
-  }
-  body {
-    height: 100vh;
-    /* background-image: url(img/1.jpg); */
-    background-size: cover;
-    background-position: center;
-    background-repeat: no-repeat;
-  }
-  
-  .container {
-    position: absolute;
-    left: 50%;
-    top: 50%;
-    transform: translate(-50%, -50%);
-    padding: 20px 25px;
-    width: 300px;
-  
-    background-color: #73e09b8f;
-    box-shadow: 0 0 10px rgba(255, 255, 255, 0.3);
-  }
-  .color {
-    color: #ffff;
-    position: absolute;
-    left: 510px;
-    top: 120px;
-    font-size: 30px;
-    background-color: red;
-  }
-  .container h1 {
-    text-align: center;
-    color: #131111;
-    margin-bottom: 30px;
-    text-transform: uppercase;
-    border-bottom: 4px solid white;
-  }
-  .container label {
-    text-align: center;
-    color: rgb(37, 19, 19);
-  }
-  .container form input {
-    width: calc(100% - 20px);
-    padding: 8px 10px;
-    margin-bottom: 15px;
-    border: none;
-    background-color: transparent;
-    border-bottom: 2px solid white;
-    color: rgb(4, 49, 29);
-    font-size: 20px;
-  }
-  .container form button {
-    width: 100%;
-    padding: 5px 0;
-    border: none;
-    background-color: black;
-    font-size: 18px;
-    color: rgb(88, 224, 133);
-  }
-
-  
-  
-`}
-</style>
-
-
-<>
-      {showForgetPassword ? (
-        <div className="container-forget">
-        <form onSubmit={ sendVerificationCode }>
-            <div className="field">
-              <label className="label">Email</label>
-              <input
-                className="input"
-                 type="email"
-                placeholder="Your Email"
-                value={email3}
-                onChange={(e) => setEmail3(e.target.value)}
-                />
-        </div>
-        <p>Already have account?{" "}<a href="#" role="button" onClick={login}>Log in here</a>.</p>
-        <button type="submit">Send Verification Code</button>
-        </form>
-        </div>
-
-      ) : ("")}
-      {showLoginPage ? (
-        <div className="container-login">
-          <h1>Login</h1>
-          <form onSubmit={loginAccount}>
-            <div className="field">
-              <label className="label">Email</label>
-              <input
-                className="input"
-                type="email"
-                placeholder="Your Email"
-                value={email2}
-                onChange={(e) => setEmail2(e.target.value)}
-              />
-              <br />
-              <label className="label">Password</label>
-              <input
-                className="input"
-                type="password"
-                placeholder="Your Password"
-                value={password2}
-                onChange={(e) => setPassword2(e.target.value)}
-              />
-            </div>
-            <p>Forgot your password?{" "}<a href="#" role="button" onClick={forgetPassword}>Reset it here</a>.</p>
-            <button type="submit">login</button>
-          </form>
-            <br></br>
-          <p>Dont Have Account?{" "}<a href="#" role="button" onClick={register}>Register here</a>.</p>
-        </div>
-      ) : ("")}
-
-      {showDaftar ? (
-            <form onSubmit={ createAccount }>
-            <div className="field">
-                <label className="label">Email</label>
-                <input
-                    className="input"
-                    type="email"
-                    placeholder="Your Email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}/>
-                    <br />
-                <label className="label">Pass</label>
-                <input
-                    className="input"
-                    type="password"
-                    placeholder="Your Password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}/>
-                </div>
-                <button type="submit">Create account</button>
-                <p>Already have account?{" "}<a href="#" role="button" onClick={login}>Log in here</a>.</p>
-
-        </form>
-        
-      ) : ("")}
-    </>
-
-{/* 
-<div class="color"></div>
-    <div class="container-login">
-      <h1>Login</h1>
-      <form onSubmit={ loginAccount }>
-        <div className="field">
-            <label className="label">Email</label>
-            <input
-                className="input"
-                type="email"
-                placeholder="Your Email"
-                value={email2}
-                onChange={(e) => setEmail2(e.target.value)}/>
-                <br />
-            <label className="label">Password</label>
-            <input
-                className="input"
-                type="password"
-                placeholder="Your Password"
-                value={password2}
-                onChange={(e) => setPassword2(e.target.value)}/>
-            </div>
-            <p>Forgot your password? <a href="" role="button" onClick={forgetPass()}>Reset it here</a>.</p>
-            <button type="submit">login</button>
-    </form>
-    </div>
-
-    <div class="container-forget">
-      <h1>Input Email</h1>
-      <form>
-        <label for="email">Email Address:</label>
-        <input type="email" id="email" name="email" required />
-        <button type="submit">Submit</button>
-      </form>
-    </div> */}
-
-
-{/* 
-    <br></br>
-    <br></br>
-    <br></br>
-    <br></br>
-    <br></br>
-    <br></br>
 
     login
     <form onSubmit={ loginAccount }>
@@ -338,7 +144,7 @@ return (
        
         </div>
         <button type="submit">Send Verification Code</button>
-      </form> */}
+      </form>
 </div>
 );
 };
